7b98beea8c0067361cf3f66d61b865314e49ce84
Victor Jost <96876377+Zarcoks@users.noreply.github.com>
2025-11-23 15:44:09 +0100

    Added my data

--- src/info/Info.js
+++ src/info/Info.js
@@ -3,7 +3,6 @@ import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -27,118 +26,107 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Victor",
+    lastName: "Jost",
+    initials: "vj", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Student in Computer Networking",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '🕺',
+            text: 'socialy fueled by dancing'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in Évry'
         },
         {
-            emoji: "💼",
-            text: "Systems Engineer at Google"
+            emoji: "🎓",
+            text: "apprentice at Télécom SudParis"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "victor.jost@telecom-sudparis.eu"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
+            link: "https://www.facebook.com/victor.jost.2025/",
             icon: 'fa fa-facebook',
             label: 'facebook'
         },
         {
-            link: "https://instagram.com",
+            link: "https://www.instagram.com/victorjost34/",
             icon: 'fa fa-instagram',
             label: 'instagram'
         },
         {
-            link: "https://github.com",
+            link: "https://github.com/Zarcoks",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/victor-jost-562420255/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Victor. I'm apprentice student at Télécom Sudparis. I also studied at the IUT of Montpellier, I enjoy swing dance, and I believe computer networking is the foundation of all our future technologies. I'm currently looking for a company for my apprenticeship.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['web', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
+            exposedTo: ['Networks (CCNA)', 'Management', 'IoT', 'AI']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'dancing',
+            emoji: '🕺'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
-        },
-        {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'fishing',
+            emoji: '🎣'
         },
         {
             label: 'cooking',
             emoji: '🌶'
+        },
+        {
+            label: 'anime',
+            emoji: '⛩️'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
 // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            title: "Coconut's website",
+            live: "https://www.lirmm.fr/coconut/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/Zarcoks", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Pokemonator",
+            live: null,
+            source: "https://github.com/Zarcoks/Pokemonator",
             image: mock2
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Travel Express",
+            live: null,
+            source: "https://github.com/AntoineCrsr/Projet-Stage-UQAC/tree/main/backend",
             image: mock3
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Cloud Automation Experiments",
+            live: null,
+            source: "https://github.com/Zarcoks/Azure-pipeline-VMSS",
             image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
         }
     ]
 }
